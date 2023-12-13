1f0b5f25d013611673a216c463ccd93e982a42f1
KoLiBer <koliberr136a1@gmail.com>
2023-12-13 18:49:24 +0330

    fix: replace self.triggers with self.triggers_replace

--- script.tf
+++ script.tf
@@ -6,37 +6,37 @@ resource "terraform_data" "this" {
   }
 
   connection {
-    type                = try(jsondecode(self.triggers.connection).type, null)
-    host                = try(jsondecode(self.triggers.connection).host, null)
-    port                = try(jsondecode(self.triggers.connection).port, null)
-    user                = try(jsondecode(self.triggers.connection).user, null)
-    password            = try(jsondecode(self.triggers.connection).password, null)
-    timeout             = try(jsondecode(self.triggers.connection).timeout, null)
-    script_path         = try(jsondecode(self.triggers.connection).script_path, null)
-    private_key         = try(jsondecode(self.triggers.connection).private_key, null)
-    certificate         = try(jsondecode(self.triggers.connection).certificate, null)
-    agent               = try(jsondecode(self.triggers.connection).agent, null)
-    agent_identity      = try(jsondecode(self.triggers.connection).agent_identity, null)
-    host_key            = try(jsondecode(self.triggers.connection).host_key, null)
-    https               = try(jsondecode(self.triggers.connection).https, null)
-    insecure            = try(jsondecode(self.triggers.connection).insecure, null)
-    use_ntlm            = try(jsondecode(self.triggers.connection).use_ntlm, null)
-    cacert              = try(jsondecode(self.triggers.connection).cacert, null)
-    bastion_host        = try(jsondecode(self.triggers.connection).bastion_host, null)
-    bastion_host_key    = try(jsondecode(self.triggers.connection).bastion_host_key, null)
-    bastion_port        = try(jsondecode(self.triggers.connection).bastion_port, null)
-    bastion_user        = try(jsondecode(self.triggers.connection).bastion_user, null)
-    bastion_password    = try(jsondecode(self.triggers.connection).bastion_password, null)
-    bastion_private_key = try(jsondecode(self.triggers.connection).bastion_private_key, null)
-    bastion_certificate = try(jsondecode(self.triggers.connection).bastion_certificate, null)
+    type                = try(jsondecode(self.triggers_replace.connection).type, null)
+    host                = try(jsondecode(self.triggers_replace.connection).host, null)
+    port                = try(jsondecode(self.triggers_replace.connection).port, null)
+    user                = try(jsondecode(self.triggers_replace.connection).user, null)
+    password            = try(jsondecode(self.triggers_replace.connection).password, null)
+    timeout             = try(jsondecode(self.triggers_replace.connection).timeout, null)
+    script_path         = try(jsondecode(self.triggers_replace.connection).script_path, null)
+    private_key         = try(jsondecode(self.triggers_replace.connection).private_key, null)
+    certificate         = try(jsondecode(self.triggers_replace.connection).certificate, null)
+    agent               = try(jsondecode(self.triggers_replace.connection).agent, null)
+    agent_identity      = try(jsondecode(self.triggers_replace.connection).agent_identity, null)
+    host_key            = try(jsondecode(self.triggers_replace.connection).host_key, null)
+    https               = try(jsondecode(self.triggers_replace.connection).https, null)
+    insecure            = try(jsondecode(self.triggers_replace.connection).insecure, null)
+    use_ntlm            = try(jsondecode(self.triggers_replace.connection).use_ntlm, null)
+    cacert              = try(jsondecode(self.triggers_replace.connection).cacert, null)
+    bastion_host        = try(jsondecode(self.triggers_replace.connection).bastion_host, null)
+    bastion_host_key    = try(jsondecode(self.triggers_replace.connection).bastion_host_key, null)
+    bastion_port        = try(jsondecode(self.triggers_replace.connection).bastion_port, null)
+    bastion_user        = try(jsondecode(self.triggers_replace.connection).bastion_user, null)
+    bastion_password    = try(jsondecode(self.triggers_replace.connection).bastion_password, null)
+    bastion_private_key = try(jsondecode(self.triggers_replace.connection).bastion_private_key, null)
+    bastion_certificate = try(jsondecode(self.triggers_replace.connection).bastion_certificate, null)
   }
 
   provisioner "remote-exec" {
     when = create
     inline = [
       "export DEBIAN_FRONTEND=noninteractive",
-      "echo '${try(jsondecode(self.triggers.connection).password, "")}' | sudo -S -v",
-      "echo ${base64encode(self.triggers.create)} | base64 -d | sudo sh"
+      "echo '${try(jsondecode(self.triggers_replace.connection).password, "")}' | sudo -S -v",
+      "echo ${base64encode(self.triggers_replace.create)} | base64 -d | sudo sh"
     ]
   }
 
@@ -44,8 +44,8 @@ resource "terraform_data" "this" {
     when = destroy
     inline = [
       "export DEBIAN_FRONTEND=noninteractive",
-      "echo '${try(jsondecode(self.triggers.connection).password, "")}' | sudo -S -v",
-      "echo ${base64encode(self.triggers.destroy)} | base64 -d | sudo sh"
+      "echo '${try(jsondecode(self.triggers_replace.connection).password, "")}' | sudo -S -v",
+      "echo ${base64encode(self.triggers_replace.destroy)} | base64 -d | sudo sh"
     ]
   }
 }
